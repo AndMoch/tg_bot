ec17ff482766e7ece3236aeda6b695bb01c250ab
AndMoch <andmochgeek@ya.ru>
2023-04-08 15:09:35 +0300

    timer 2.0

--- main.py
+++ main.py
@@ -27,14 +27,45 @@ async def date_show(update, context):
     await update.message.reply_text(f"{datetime.strftime(date.today(), f)}")
 
 
+def remove_job_if_exists(name, context):
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def set_timer(update, context):
+    chat_id = update.effective_message.chat_id
+    seconds = int(context.args[0])
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, seconds, chat_id=chat_id, name=str(chat_id), data=seconds)
+
+    text = f'Вернусь через {seconds} с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def task(context):
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {context.job.data}c. прошли!')
+
+
+async def unset(update, context):
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(echo_handler)
-    time_handler = CommandHandler("time", time_show)
-    application.add_handler(time_handler)
-    date_handler = CommandHandler("date", date_show)
-    application.add_handler(date_handler)
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(CommandHandler("time", time_show))
+    application.add_handler(CommandHandler("date", date_show))
+    application.add_handler(CommandHandler("set_timer", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
     application.run_polling()
 
 
